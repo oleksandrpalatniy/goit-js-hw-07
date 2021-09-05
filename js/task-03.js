5e77f1d0b1c488bd457a94f37924b46f87722c74
Oleksandr Palatniy <oleksandr.palatniy@gmail.com>
2021-09-05 22:34:42 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -1,15 +1,15 @@
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    alt: 'White and Black Long Fur Cat'
   },
   {
     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish'
   },
   {
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    alt: 'Group of Horses Running'
   },
   
 ];
@@ -19,9 +19,9 @@ const ulList = document.getElementById('gallery')
 ulList.className = 'images__list'
 
 const elements = images.map(value => {
-  const imageList = `<li> <img class="images" src='${value.url}' alt='${value.alt}'> </li>`
+  const imageList = `<li> <img class="images" src=${value.url} alt=${value.alt}> </li>`
   return imageList
-})
 
-ulList.insertAdjacentHTML('afterbegin', elements)
+})
+ulList.insertAdjacentHTML('afterbegin', elements.join(''))
 
